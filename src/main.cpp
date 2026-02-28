6be652d905b3d54e387aae5d79a24600f91991a4
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2026-02-28 18:46:06 -0500

    experimenting with perlin noise and other aspects of open gl

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,23 @@
 #include <shader.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/type_ptr.hpp>fd
 #include <texture.hpp>
+#include <chrono>
+#include <print>
+#include <FastNoiseLite.h>
 
 void framebuffer_size_callback(GLFWwindow *, int width, int height);
 static void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+auto lastFrameTime = std::chrono::steady_clock::now();
+float cubeRotation = 0.0f;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 int main(void)
 {
@@ -51,12 +60,33 @@ int main(void)
 
     Shader ourShader("shaders/vertex.glsl", "shaders/fragment.glsl");
 
-    glm::vec3 cubePositions[20] = {};
-    for (int i = 0; i < 20; i++)
+    FastNoiseLite noise;
+    noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+
+    // Gather noise data
+    float noiseInfo[128 * 128];
+    int index = 0;
+
+    for (int y = 0; y < 128; y++)
     {
-        float x = (i / 20.0f);
-        cubePositions[i] = glm::vec3(x, 0.0f, -2.0f);
+        for (int x = 0; x < 128; x++)
+        {
+            noiseInfo[index++] = noise.GetNoise((float)x, (float)y);
+        }
     }
+
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     // Set vertices and create Buffer object
     // ------------------------------------------------------------------
@@ -101,7 +131,8 @@ int main(void)
         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
         0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
         -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+        };
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -119,17 +150,29 @@ int main(void)
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    Texture texture1{"assets/textures/wood.jpg"};
-    Texture texture2{"assets/textures/awesomeface.png"};
+    Texture texture1{"assets/textures/dirt.jpg"};
+    // Texture texture2{"assets/textures/awesomeface.png"};
 
     ourShader.use();
 
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
+    
+    glPointSize(10);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        // delta time
+        auto currentFrameTime = std::chrono::steady_clock::now();
+
+        float deltaTime =
+            std::chrono::duration<float>(currentFrameTime - lastFrameTime).count();
+
+        lastFrameTime = currentFrameTime;
+
+        // std::print("DeltaTime:{}\n", deltaTime);
+
         /*Process input*/
         processInput(window);
         /* Render here */
@@ -137,10 +180,10 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Bind Textures
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1.GetTexture());
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2.GetTexture());
+        // glActiveTexture(GL_TEXTURE0);
+        // glBindTexture(GL_TEXTURE_2D, texture1.GetTexture());
+        // glActiveTexture(GL_TEXTURE1);
+        // glBindTexture(GL_TEXTURE_2D, texture2.GetTexture());
 
         ourShader.use();
 
@@ -148,9 +191,10 @@ int main(void)
         glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.5f, 100.0f);
+
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
@@ -161,16 +205,19 @@ int main(void)
         ourShader.setMat4("projection", projection);
 
         glBindVertexArray(VAO);
-        for (unsigned int i = 0; i < 10; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        // for (unsigned int i = 0; i < 10; i++)
+        // {
+        //     glm::mat4 model = glm::mat4(1.0f);
+        //     model = glm::translate(model, noiseData[i]);
+        //     float angle = 20.0f * i;
+        //     model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        //     ourShader.setMat4("model", model);
+
+        //     glDrawArrays(GL_TRIANGLES, 0, 36);
+        // }
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -186,21 +233,21 @@ int main(void)
     return 0;
 }
 
-// static void processInput(GLFWwindow *window)
-// {
-//     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-//         glfwSetWindowShouldClose(window, true);
+static void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 
-//     const float cameraSpeed = 0.05f; // adjust accordingly
-//     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-//         cameraPos += cameraSpeed * cameraFront;
-//     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-//         cameraPos -= cameraSpeed * cameraFront;
-//     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-//         cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-//     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-//         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-// }
+    const float cameraSpeed = 0.05f; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
 
 void framebuffer_size_callback(GLFWwindow *, int width, int height)
 {
